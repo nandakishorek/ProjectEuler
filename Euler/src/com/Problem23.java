f3ffccb572d3e0cf94d6ff516540bd8351124505
Nandakishore K <nandakishorek17@gmail.com>
2015-05-29 10:29:12 +0530

    Refactored solution 23

--- Euler/src/com/Problem23.java
+++ Euler/src/com/Problem23.java
@@ -30,6 +30,10 @@ import java.util.Set;
  *
  */
 public class Problem23 {
+    
+    static final int MIN_ABUNDANT = 12;
+    static final int LIMIT = 28124;
+    static final int SIGMA_TWENTY_THREE = 276;
 
     /**
      * A brute force solution - 
@@ -46,32 +50,17 @@ public class Problem23 {
         // find all abundant numbers from 13 to 28123, since 12 is the smallest
         // abundant number and greatest number that cannot be expressed as the
         // sum of two abundant numbers is less than 28123
-        abundantNumSet.add(12);
-        for (int i = 13; i < 28124; ++i) {
+        abundantNumSet.add(MIN_ABUNDANT);
+        for (int i = MIN_ABUNDANT+1; i < LIMIT; ++i) {
             if (isAbundant(i)) {
                 abundantNumSet.add(i);
             }
         }
 
-        int total = 276; // sigma 23, since 24 = 12 * 2, minor and unnecessary optimization
-        for (int i = 25; i < 28124; ++i) {
-            boolean isRep = false;
-
-            for (Integer num : abundantNumSet) {
-                // iterating through half of the numbers in the set is enough
-                if (abundantNumSet.contains(i - num)) {
-                    // the number can be represented as a sum of two
-                    isRep = true;
-                    break;
-                }
-                if (i < num) {
-                    // the abundant number should less than a given number to
-                    // be part of the numbers forming the sum
-                    break;
-                }
-            }
-
-            if (!isRep) {
+        int total = SIGMA_TWENTY_THREE; // sigma 23, since 24 = 12 * 2, minor and unnecessary optimization
+        for (int i = (MIN_ABUNDANT * 2); i < LIMIT; ++i) {
+            
+            if (!isSumOfTwoAbundants(i, abundantNumSet)) {
                 // the number cannot be represented as sum of two abundant
                 // numbers
                 total += i;
@@ -79,6 +68,21 @@ public class Problem23 {
         }
         
         System.out.println(total);
+    }
+    
+    public static boolean isSumOfTwoAbundants(int num, Set<Integer> abundantNumSet) {
+        for (Integer i : abundantNumSet) {
+            if (abundantNumSet.contains(num - i)) {
+                // the number can be represented as a sum of two
+                return true;
+            }
+            if (i > num) {
+                // the abundant number should less than a given number to
+                // be part of the numbers forming the sum
+                break;
+            }
+        }
+        return false;
     }
 
     public static boolean isAbundant(int n) {
@@ -92,16 +96,12 @@ public class Problem23 {
                 if (quotient != i) {
                     sum += i + quotient; // add the factors
                 } else {
-                 // avoid adding twice - factor in case of perfect squares
+                    // avoid adding twice - factor in case of perfect squares
                     sum += quotient;
                 }
             }
         }
 
-        if (sum > n) {
-            return true;
-        }
-
-        return false;
+        return (sum > n);
     }
 }
